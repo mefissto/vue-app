251e1a9ddbef095ef12c4ef5460123f2d48f2441
mefissto <mefissto.w@gmail.com>
2020-07-25 20:45:32 +0300

    add router

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque et enim cum nobis fugiat fuga iusto iure consequatur ipsa beatae?</p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
---
+++ src/views/Todo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Todos</h2>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+};
+</script>
